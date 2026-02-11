5972edda7c551566a749b261f7db0ae22f804489
VoidXDev <developershiva18@gmail.com>
2026-02-11 19:46:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ except ValueError:
     FSUB_CHANNEL_IDS = []
 
 # --- 💎 WITHDRAWAL CONFIG 💎 ---
-COUPON_COSTS = {500: 1, 1000: 5, 2000: 15, 4000: 25}
+COUPON_COSTS = {500: 1, 1000: 5, 2000: 25, 4000: 35}
 
 # States for Admin Conversation
 WAITING_FOR_COUPONS = 1
@@ -398,7 +398,7 @@ async def withdraw_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
     keyboard = [
         [InlineKeyboardButton("1 💎 = 500 🎟 ", callback_data="redeem_500"), InlineKeyboardButton("5 💎 = 1000 🎟 ", callback_data="redeem_1000")],
-        [InlineKeyboardButton("15 💎 = 2000 🎟 ", callback_data="redeem_2000"), InlineKeyboardButton("25 💎 = 4000 🎟 ", callback_data="redeem_4000")],
+        [InlineKeyboardButton("25 💎 = 2000 🎟 ", callback_data="redeem_2000"), InlineKeyboardButton("35 💎 = 4000 🎟 ", callback_data="redeem_4000")],
         [InlineKeyboardButton("🔙 Back", callback_data="close_withdraw")]
     ]
     await update.message.reply_text(text, parse_mode=ParseMode.HTML, reply_markup=InlineKeyboardMarkup(keyboard))
